eb94df3e8e791cb096a3c8457f7c7fce16788ded
Evan <evanfujita@gmail.com>
2020-12-15 22:51:49 -0600

    commit tree 70a842bee35c1aecaf2a37336dadbb8a7b3e97c8

---
+++ lib/backer.rb
@@ -0,0 +1,21 @@
+
+
+class Backer
+
+    attr_reader :name
+
+    def initialize(name)
+        @name = name 
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+
+    def backed_projects
+        project = ProjectBacker.all.select {|pbs| pbs.backer == self}
+        project.map {|pb| pb.project}
+    end
+
+
+end
